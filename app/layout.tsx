6dd23293db68958ad62dbcee355b2c6e4cb4cb7b
Kartikey <srikartikey11@gmail.com>
2025-08-15 00:38:45 +0530

    vercel error final fix font remove

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,12 @@
 
 import "./globals.css";
 import { AuthProvider } from "@/context/auth-context"; // adjust path if needed
-import localFont from "next/font/local";
 
-const inter = localFont({
-  src: [
-    { path: "../assets/fonts/inter/Inter-Regular.ttf", weight: "400", style: "normal" },
-    { path: "../assets/fonts/inter/Inter-Italic.ttf", weight: "400", style: "italic" },
-  ],
-  display: "swap",
-});
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en">
       <body>
         <AuthProvider>
           {children}
